48db5795a4e8af176815e5c60f9a5eab12d0b317
Tom De Vylder <tom@penumbra.be>
2012-11-20 15:50:23 +0100

    Move templates to $::osfamily folder

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,17 +17,22 @@ class proftpd::config {
       ensure => directory;
 
     "${::proftpd::config_dir}/proftpd.conf":
-      content => template("proftpd/proftpd.conf.${::osfamily}.erb");
+      content => template("proftpd/${::osfamily}/proftpd.conf.erb");
 
-    "${::proftpd::config_dir}/modules.conf":;
+    "${::proftpd::config_dir}/modules.conf":
+      content => template("proftpd/${::osfamily}/modules.conf.erb");
 
-    "${::proftpd::config_dir}/sql.conf":;
+    "${::proftpd::config_dir}/sql.conf":
+      content => template("proftpd/${::osfamily}/sql.conf.erb");
 
-    "${::proftpd::config_dir}/tls.conf":;
+    "${::proftpd::config_dir}/tls.conf":
+      content => template("proftpd/${::osfamily}/tls.conf.erb");
 
-    "${::proftpd::config_dir}/virtuals.conf":;
+    "${::proftpd::config_dir}/virtuals.conf":
+      content => template("proftpd/${::osfamily}/virtuals.conf.erb");
 
-    "${::proftpd::config_dir}/ldap.conf":;
+    "${::proftpd::config_dir}/ldap.conf":
+      content => template("proftpd/${::osfamily}/ldap.conf.erb");
 
   }
 
